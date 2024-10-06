9656d42058cb586fbc2e28f65b2f90599d686174
Balakrishnan T <126885624+balakrishnanthirumal@users.noreply.github.com>
2024-10-06 13:03:58 +0530

    Delete MeowMeowWoof_v2_complete directory

--- MeowMeowWoof_v2_complete/background.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ bookmarks: [] });
-    console.log("Extension installed and storage initialized.");
-});
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({ url: 'popup.html' });
-});
